de02cc11b0ece97eda0afa2abf13192539ea4dbf
Алексей Мухин <mukhinalexeym@gmail.com>
2017-02-17 23:09:21 +0300

    WIP: Writing grammar and tests

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # java-swift
+
+Code metrics counter in Java for Apple Swift 3.0 using Java CC
---
+++ src/Parser.java
@@ -0,0 +1,39 @@
+import com.sun.javafx.font.Metrics;
+
+public class Parser implements MetricsProtocol {
+
+    @Override
+    public int linesOfCodeCount() {
+        return 0;
+    }
+
+    @Override
+    public int methodsCount() {
+        return 0;
+    }
+
+    @Override
+    public int attributesCount() {
+        return 0;
+    }
+
+    @Override
+    public double weightedMethodComplexity() {
+        return 0;
+    }
+
+    @Override
+    public double classComplexity() {
+        return 0;
+    }
+
+    @Override
+    public int coupledToOtherClassesCount() {
+        return 0;
+    }
+
+    @Override
+    public int abstractDataTypesInClassCount() {
+        return 0;
+    }
+}
